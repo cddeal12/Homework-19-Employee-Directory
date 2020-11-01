ddb9b3d88282f07227340314f41487ad090a19ce
Nemancer <cddeal12@gmail.com>
2020-10-31 19:49:20 -0700

    Finished adding component bootstrap

--- employee-directory/src/components/ButtonsBar.js
+++ employee-directory/src/components/ButtonsBar.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function ButtonsBar(props) {
     return (
-        <nav>
-            <button className="button px-auto" onClick={props.byName}>Sort By Name</button>
-            <button className="button mx-auto" onClick={props.byPosition}>Sort By Position</button>
-            <button className="button mx-auto" onClick={props.filterSales}>Filter By Salespeople</button>
-            <button className="button mx-auto" onClick={props.filterManagers}>Filter By Managers</button>
-        </nav>
+        <div>
+            <button type="button" className="btn btn-secondary mx-3" onClick={props.byName}>Sort By Name</button>
+            <button type="button" className="btn btn-secondary mx-3" onClick={props.byPosition}>Sort By Position</button>
+            <button type="button" className="btn btn-secondary mx-3" onClick={props.filterSales}>Filter By Salespeople</button>
+            <button type="button" className="btn btn-secondary mx-3" onClick={props.filterManagers}>Filter By Managers</button>
+        </div>
     )
 }
 
--- employee-directory/src/components/EmployeeTable.js
+++ employee-directory/src/components/EmployeeTable.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function EmployeeTable(props) {
     return (
-        <div className="row">
+        <div>
             {props.employees.map(employee => (
-                <div className="col-10 border my-4">
-                    <h3>{employee.name}</h3>
-                    <img alt={employee.name} src={employee.image} className="border rounded"/>
-                    <h4>{employee.position}</h4>
+                <div className="row border my-4">
+                    <img className="col-4 border rounded" alt={employee.name} src={employee.image}/>
+                    <h3 className="col-4">{employee.name}</h3>
+                    <h4 className="col-4">{employee.position}</h4>
                 </div>
             ))}
         </div>
